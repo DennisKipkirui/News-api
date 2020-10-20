d66bea646545c666bf9ce2e5f1407e9976320581
Dennis <denismutai95@gmail.com>
2020-10-20 23:06:50 +0300

    fix bugs

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,26 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
 
+bootstrap = Bootstrap()
 # Initializing application
-app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
 
-from app import views
+    #  Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
+    
+    #Initializing flask extensions
+    bootstrap.init_app(app)
+
+    return app
+
+
