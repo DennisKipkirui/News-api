09b74d143625d604401c4402505a277920fa798c
Dennis <denismutai95@gmail.com>
2020-10-20 22:39:10 +0300

    add classes

---
+++ app/models.py
@@ -0,0 +1,29 @@
+class Sources:
+    """
+    Source class to define the source of news
+    """
+    
+    def __init__(self, id, name, description, url, newsCategory, language, country):
+        self.id= id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.newsCategory = newsCategory
+        self.language = language
+        self.country = country
+        
+        
+class Articles:
+    """
+    class to define the news article
+    """
+    
+    def __init__(self,id,title,author,description,url,urlToImage,publishedAt,content):
+        self.id = id
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
